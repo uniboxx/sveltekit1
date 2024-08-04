b8246ae24431070f5c7fec461741f85a0ada56f6
uniboxx <unibox@duck.com>
2024-08-04 22:07:28 +0200

    added layout.svelte

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<script>
+  import { base } from '$app/paths';
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<nav>
+  <a href="{base}/">Home</a>
+  <a href="{base}/about">About</a>
+</nav>
+<p>
+  Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+</p>
+
+<slot />
--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,6 +1,1 @@
-<script>
-  import { base } from '$app/paths';
-</script>
-
-<a href="{base}/">Home</a>
 <h1>About</h1>
