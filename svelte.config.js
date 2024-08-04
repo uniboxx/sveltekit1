5c3a65b5f430cb13c0e36fa4cd2cbbf8dad0f91d
uniboxx <unibox@duck.com>
2024-08-04 21:35:39 +0200

    changed base

--- svelte.config.js
+++ svelte.config.js
@@ -10,7 +10,7 @@ const config = {
       fallback: 'index.html',
     }),
     paths: {
-      base: process.env.NODE_ENV === 'production' ? '/svelte-routing' : '',
+      base: process.env.NODE_ENV === 'production' ? '/sveltekit' : '',
     },
   },
 };
